d789e23fdf4a8b281c9b6ab56d07c0b9d1d71865
Koushik <55914356+koushik147@users.noreply.github.com>
2022-10-10 17:04:24 -0400

    Add files via upload

---
+++ DynamicProgramming2/Coin_Change.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [[0 for i in range(amount+1)] for j in range(len(coins)+1)] # creating dp matrix
+        
+        for j in range(1,amount+1): 
+            dp[0][j] = amount+1 # assigning amount+1 to dp array
+        
+        for i in range(1,len(coins)+1):
+            for j in range(amount+1):
+                if j < coins[i-1]: # if jth value is less than coins
+                    dp[i][j] = dp[i-1][j] 
+                else:
+                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]]) # assigning min value between dp value and dp of coins
+                    
+        if dp[-1][-1] == amount+1: # if dp of last is equal to amount+1
+            return -1
+        return dp[-1][-1] # returning last value in matrix
---
+++ DynamicProgramming2/Knapsack1.py
@@ -0,0 +1,15 @@
+def knapsack(val,wt,c,n):
+    dp = [[ 0 for i in range(c+1)] for j in range(n+1)] # creating dp matrix with value 0
+    for i in range(1,n+1):
+        for j in range(c+1):
+            if j<wt[i-1]: # if jth isterative value is less than weight
+                dp[i][j] = dp[i-1][j] # assigning to dp martix
+            else:
+                dp[i][j] = max(dp[i-1][j], val[i-1]+dp[i-1][j-wt[i-1]]) # finding max between previous and weight value + dp array and assigning to dp matrix
+    return dp[-1][-1]
+
+val = [2,4,6,8]
+wt = [1,2,3,4]
+c=7
+n=len(val)
+print(knapsack(val,wt,c,n))
